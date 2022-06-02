c9255dc5369b37103b7f718cdae1d17158a69a6c
Patricia <patricia.onieva@gmail.com>
2022-06-02 19:58:38 -0300

    Primer Commit Index

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require('express');
+const path = require('path')
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000,()=> console.log('Server Levantado en Port 3000'));
+
+app.get('/under_constr', (req, res)=>{
+		res.sendFile(path.join(__dirname, '/views/under_constr.html'));
+	});
+
+app.get('/', (req, res)=>{
+		res.sendFile(path.join(__dirname, '/views/index.html'));
+	}
+	)
+
+	app.get('/login', (req, res)=>{
+		res.sendFile(path.join(__dirname, '/views/login.html'));
+	}
+	)	
+
+	app.get('/productCart', (req, res)=>{
+		res.sendFile(path.join(__dirname, '/views/productCart.html'));
+	}
+	)	
+
+	app.get('/productDetail', (req, res)=>{
+		res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+	}
+	)	
+	
+	app.get('/register', (req, res)=>{
+		res.sendFile(path.join(__dirname, '/views/register.html'));
+	}
+	)	
