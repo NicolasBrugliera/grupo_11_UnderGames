81b4ba6f1cee1575c30f0d60c82fd7958b5d9d39
Patricia <patricia.onieva@gmail.com>
2022-06-02 20:13:46 -0300

    Agregado datos para Publicar en Heroku

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000,()=> console.log('Server Levantado en Port 3000'));
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Server Levantado en Port 3000');
+})
 
 app.get('/under_constr', (req, res)=>{
 		res.sendFile(path.join(__dirname, '/views/under_constr.html'));
