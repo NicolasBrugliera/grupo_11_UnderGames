42975e754981f2511f7c6a385d75662833e37a17
Sebastian Alonso <sebajorgealonso@gmail.com>
2022-08-07 17:24:30 -0300

    Agregado lista rank carrito

--- src/controllers/controllerProductCart.js
+++ src/controllers/controllerProductCart.js
@@ -1,9 +1,10 @@
 const path = require ('path');
-
+const fs = require('fs')
 
 const productCart ={
     productCart: (req, res)=>{
-        res.render(path.join(__dirname, '../views/productCart.ejs'))
+        let data_games3 = JSON.parse(fs.readFileSync(path.join(__dirname, '../baseDeDatos/data_games3.json')))
+        res.render(path.join(__dirname, '../views/productCart.ejs'),{data_games3})
     }
 }
 
