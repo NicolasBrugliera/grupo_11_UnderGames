d0326656736b5567a7dce9798f24a5b6bc612b3f
Santiago <santiso@gmail.com>
2022-08-28 18:48:42 -0300

    productDetail con BD

--- src/controllers/controllerProductDetail.js
+++ src/controllers/controllerProductDetail.js
@@ -1,19 +1,16 @@
 const path = require ('path');
 const fs = require('fs');
+const db = require('../database/models');
 
 
 module.exports = {
-    productDetail: (req,res) =>{
-        let products =  JSON.parse(fs.readFileSync(path.join(__dirname,'../baseDeDatos/data_games3.json')));
-        let selectedGame;
-        products.forEach(game => {
-            if(game.id == req.params.id){
-            selectedGame = game;
-            }
-        });
-   
-    res.render(path.resolve(__dirname, '../views/productDetail/'), {selectedGame})
-
-    }
+    'productDetail': (req, res) => {
+        let id_game = req.params.id;
+        db.Game.findByPk(id_game, {include: ['category', 'game_group', 'platform','graphics_min', 'graphics_rec', 'processor_min', 'processor_rec', 'os_min', 'os_rec', 'storage_min', 'storage_rec', 'creator'] })
+            .then(game_id => {
+                selectedGame = game_id
+                res.render(path.join(__dirname, '../views/productDetail'),{selectedGame})
+            });
+        }
 
 }
