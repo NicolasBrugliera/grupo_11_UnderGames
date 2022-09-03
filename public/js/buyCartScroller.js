be23c2d7b68d4946b734c63e0d8a61cc2bcae9bf
Santiago <santiso@gmail.com>
2022-09-03 20:26:25 -0300

    arreglos en productDetail

--- public/js/buyCartScroller.js
+++ public/js/buyCartScroller.js
@@ -2,14 +2,33 @@
 let buyCart = document.querySelector('.cart-section__PD')
 
 window.addEventListener("scroll",function() { 
-if(window.scrollY >= 410 && window.innerWidth >= 768) { 
+if(window.scrollY >= 470 && window.innerWidth > 768) { 
     buyCart.style.position = 'absolute'; 
-    buyCart.style.top = '75%'; 
+    buyCart.style.top = '80%'; 
 } 
-if(window.scrollY <= 410 && window.innerWidth >= 768){ 
+if(window.scrollY <= 450 && window.innerWidth > 768){ 
     buyCart.style.position = 'fixed'; 
     buyCart.style.top = '25%'; 
 } })
 
 
   
+ window.addEventListener("scroll",function() { 
+    if(window.scrollY >= 500 && window.innerHeight > 1365) { 
+        buyCart.style.position = 'absolute'; 
+        buyCart.style.top = '95%'; 
+    } 
+    if(window.scrollY <= 500 && window.innerHeight > 1365){ 
+        buyCart.style.position = 'fixed'; 
+        buyCart.style.top = '15%'; 
+    } }) 
+
+window.addEventListener("scroll",function() { 
+    if(window.scrollY >= 450 && window.innerHeight > 1024 && window.innerWidth > 768) { 
+        buyCart.style.position = 'absolute'; 
+        buyCart.style.top = '50%'; 
+    } 
+    if(window.scrollY <= 450 &&  window.innerHeight > 1024 && window.innerWidth > 768){ 
+        buyCart.style.position = 'fixed'; 
+        buyCart.style.top = '16%'; 
+    } }) 
