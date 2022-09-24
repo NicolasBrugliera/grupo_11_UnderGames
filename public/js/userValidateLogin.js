93cbacd747931adf53aa66b7cbd5ac165593e566
Sebastian Alonso <sebajorgealonso@gmail.com>
2022-09-24 16:30:07 -0300

    arreglos validacion front user y register - Cart

---
+++ public/js/userValidateLogin.js
@@ -0,0 +1,122 @@
+
+window.onload = function () {
+
+    let form = document.querySelector('.log-in-form');
+    form.email.focus()
+
+
+
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        let errors = []
+
+        let email = document.querySelector('[name="email"]')        
+        let password = document.querySelector('[name="password"]')
+       
+        
+
+        //validacion email
+        if (email.value == '') {
+            errors.push('El campo email no puede estar vacio!')
+            email.classList.add('is-invalid_login')
+
+        }
+        
+        else {
+
+            email.classList.add('is-valid_login')
+            email.classList.remove('is-invalid_login')
+        }
+
+        //validacion Password
+        if (password.value == '') {
+            errors.push('El campo password no puede estar vacio!')
+            password.classList.add('is-invalid_login')
+        }
+        
+
+        else {
+            password.classList.add('is-valid_login')
+            password.classList.remove('is-invalid_login')
+        }
+        
+        //validacion password length
+        if (password.value.length <= 7 ) {
+            errors.push('La contraseña deben tener 8 caracteres como minimo')
+            password.classList.add('is-invalid_login')
+        }
+        
+
+        else {
+            password.classList.add('is-valid_login')
+            password.classList.remove('is-invalid_login')
+        }
+    
+        
+
+        let ulErrors = document.querySelector('.errors');
+        ulErrors.classList.add('alert-warning_login')
+        ulErrors.innerHTML = '';
+        if (errors.length > 0) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Revise los errores!',
+            })
+            for (let i = 0; i < errors.length; i++) {
+                ulErrors.innerHTML += `<li > ${errors[i]} </li>`
+            }
+        }
+        else {
+        
+            const body = {
+                
+                email: e.target.email.value,                
+                password: e.target.password.value,
+            }
+            
+            form.submit()
+            // POST
+        //   let  handleGetJson = () => {
+        //         fetch(`/users/register`, {
+        //             method: 'POST',
+        //             headers: {
+        //                 'Content-Type': 'application/json',
+        //                 'Accept': 'application/json'
+        //             }
+        //         })
+        //             .then(res => res.json())
+        //             .then(data => console.log(data))
+        //     }
+           
+            // const fetchResponse = await fetch("api/users/create", {
+            //     method: 'POST',
+            //     headers: { "Content-Type": "application/json" },
+            //     body: JSON.stringify(body)
+            // })
+            
+            // let res = await fetchResponse.json()
+            // if (res.meta.status == 200) {
+            //     ulErrors.innerHTML = '';
+            //     Swal.fire(
+            //         'Muy bien!',
+            //         'Te registraste',
+
+            //         'success'
+            //     )
+            // }
+            // else {
+            //     Swal.fire({
+            //         icon: 'error',
+            //         title: 'Oops...',
+            //         text: 'Hubo un error al registrarse!',
+            //     })
+            // }
+        }
+
+
+    })
+
+}
+
--- public/js/userValidateRegister.js
+++ public/js/userValidateRegister.js
@@ -1,120 +1,117 @@
 
 window.onload = function () {
 
-    let form = document.querySelector('.register-form');
+    const form = document.querySelector('.register-form');
     form.name.focus()
-
-
-
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault()
         let errors = []
 
-        let tos = document.querySelector('#tos')
-        let name = document.querySelector('#name')
-        let surname = document.querySelector('#surname')
-        let email = document.querySelector('#email')
-        let gamertag = document.querySelector('#gamertag')
-        let password = document.querySelector('#password')
-        let repetedpassword = document.querySelector('#repeted-password')
+        // let tos = document.querySelector('#tos')
+        let name = document.querySelector('[name="name"]')
+        let surname = document.querySelector('[name="surname"]')
+        let email = document.querySelector('[name="email"]')
+        let gamertag = document.querySelector('[name="gamertag"]')
+        let password = document.querySelector('[name="password"]')
+        let repetedpassword = document.querySelector('[name="repeted-password"]')
         
         //validacion Nombre
         if (name.value == '') {
             errors.push('El campo Nombre no puede estar vacio!')
-            name.classList.add('is-invalid')
+            name.classList.add('is-invalid_registerUser')
         }
         else {
-            name.classList.add('is-valid')
-            name.classList.remove('is-invalid')
+            name.classList.add('is-valid_registerUser')
+            name.classList.remove('is-invalid_registerUser')
         }
         //validacion tos
         // if (tos.value !== "on" ) {
         //     errors.push('El campo tos no puede estar vacio!')
-        //     tos.classList.add('is-invalid')
+        //     tos.classList.add('is-invalid_registerUser')
         // }
         // else {
-        //     tos.classList.add('is-valid')
-        //     tos.classList.remove('is-invalid')
+        //     tos.classList.add('is-valid_registerUser')
+        //     tos.classList.remove('is-invalid_registerUser')
         // }
         //validacion apellido
         if (surname.value == '') {
             errors.push('El campo surname no puede estar vacio!')
-            surname.classList.add('is-invalid')
+            surname.classList.add('is-invalid_registerUser')
         }
         else {
-            surname.classList.add('is-valid')
-            surname.classList.remove('is-invalid')
+            surname.classList.add('is-valid_registerUser')
+            surname.classList.remove('is-invalid_registerUser')
         }
         //validacion email
         if (email.value == '') {
             errors.push('El campo email no puede estar vacio!')
-            email.classList.add('is-invalid')
+            email.classList.add('is-invalid_registerUser')
 
         }
         
         else {
 
-            email.classList.add('is-valid')
-            email.classList.remove('is-invalid')
+            email.classList.add('is-valid_registerUser')
+            email.classList.remove('is-invalid_registerUser')
         }
         //validacion gamertag
         if (gamertag.value == '') {
             errors.push('El campo gamertag no puede estar vacio!')
-            gamertag.classList.add('is-invalid')
+            gamertag.classList.add('is-invalid_registerUser')
         }
         else {
-            gamertag.classList.add('is-valid')
-            gamertag.classList.remove('is-invalid')
+            gamertag.classList.add('is-valid_registerUser')
+            gamertag.classList.remove('is-invalid_registerUser')
 
         }
         //validacion Password
         if (password.value == '') {
             errors.push('El campo password no puede estar vacio!')
-            password.classList.add('is-invalid')
+            password.classList.add('is-invalid_registerUser')
         }
         
 
         else {
-            password.classList.add('is-valid')
-            password.classList.remove('is-invalid')
+            password.classList.add('is-valid_registerUser')
+            password.classList.remove('is-invalid_registerUser')
         }
         //validacion password match
         if (password.value !== repetedpassword.value ) {
             errors.push('Las contraseñas no son iguales!')
-            password.classList.add('is-invalid')
+            password.classList.add('is-invalid_registerUser')
         }
         
 
         else {
-            password.classList.add('is-valid')
-            password.classList.remove('is-invalid')
+            password.classList.add('is-valid_registerUser')
+            password.classList.remove('is-invalid_registerUser')
         }
         //validacion password length
-        if (password.value.length <= 5 || repetedpassword.value.length <= 5 ) {
-            errors.push('Las contraseñas deben tener 6 caracteres como minimo!')
-            password.classList.add('is-invalid')
+        if (password.value.length <= 7 || repetedpassword.value.length <= 7 ) {
+            errors.push('Las contraseñas deben tener 8 caracteres como minimo!')
+            password.classList.add('is-invalid_registerUser')
         }
         
 
         else {
-            password.classList.add('is-valid')
-            password.classList.remove('is-invalid')
+            password.classList.add('is-valid_registerUser')
+            password.classList.remove('is-invalid_registerUser')
         }
     
         
         //validacion repeted-Password
         if (repetedpassword.value == '') {
             errors.push('El campo repeted Password no puede estar vacio!')
-            repetedpassword.classList.add('is-invalid')
+            repetedpassword.classList.add('is-invalid_registerUser')
         }
         else {
-            repetedpassword.classList.add('is-valid')
-            repetedpassword.classList.remove('is-invalid')
+            repetedpassword.classList.add('is-valid_registerUser')
+            repetedpassword.classList.remove('is-invalid_registerUser')
         }
       
-        let ulErrors = document.querySelector('.errores');
-        ulErrors.classList.add('alert-warning')
+        let ulErrors = document.querySelector('.errors');
+        ulErrors.classList.add('alert-warning_register')
         ulErrors.innerHTML = '';
         if (errors.length > 0) {
             Swal.fire({
@@ -153,7 +150,7 @@ window.onload = function () {
         //             .then(data => console.log(data))
         //     }
            
-            // const fetchResponse = await fetch("/users/register", {
+            // const fetchResponse = await fetch("api/users/create", {
             //     method: 'POST',
             //     headers: { "Content-Type": "application/json" },
             //     body: JSON.stringify(body)
